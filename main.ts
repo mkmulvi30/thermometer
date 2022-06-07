c71f19718bd7af2703b858abc775d07383aa32ff
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-07 14:33:32 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
-let temp = input.temperature()
-basic.showNumber(temp)
+let temp = 0
+// If the temp is too hot or too cold, show an led alert
+// 
 basic.forever(function () {
-	
+    temp = input.temperature()
+    basic.showNumber(temp)
+    if (temp > 30) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (temp < 0) {
+        basic.showIcon(IconNames.Surprised)
+    }
 })
